231ab8414a621176dcf0d7733310876cd64d2566
uDaniAlves <daniel.augusto.balves@gmail.com>
2021-04-12 19:27:51 +0100

    Teste gitLens

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,14 +17,36 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       const {email} = user; 
-
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email } }
+      console.log(user)
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
         )
-      )
-      return true;
+        return true
+      } catch {
+        return false
+      }
+
+      
     },
   }
 })
